98de14d71c6f3a843a1fa6489d144977740a47ba
Crowdbotics <team@crowdbotics.com>
2021-11-05 22:05:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/labahns_landscaping_31844/MainActivity.java
@@ -0,0 +1,15 @@
+package com.labahns_landscaping_31844;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "labahns_landscaping_31844";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "labahns_landscaping_31844",
+  url: "https://labahns_landscaping_31844.botics.co",
+  api: "https://labahns_landscaping_31844.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
